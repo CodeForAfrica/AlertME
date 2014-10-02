b4d804550cea2d21e1b3d7a53ff91229b4fa6299
David Lemayian <david@codeforafrica.org>
2014-10-02 05:50:36 +0300

    Keeping assets local
    This is until we are able to optimise using cdn or plus caching.

--- app/views/layouts/base.blade.php
+++ app/views/layouts/base.blade.php
@@ -13,13 +13,13 @@
     <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
     <link rel="shortcut icon" href="/assets/img/favicon.ico">
 
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="/assets/css/flat-ui.min.css">
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/assets/css/font-awesome.min.css" rel="stylesheet">
 
     @yield('stylesheets')
 
-    <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+    <script src="/assets/js/vendormodernizr-2.6.2.min"></script>
 
   </head>
   <body class="@yield('body-class')">
@@ -40,7 +40,6 @@
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery-2.1.1.min.js"><\/script>')</script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min.js"></script>
 
     <script src="/assets/js/plugins.js"></script>
     <script src="/assets/js/application.js"></script>
