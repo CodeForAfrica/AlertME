41cf0648a14865dd3dd5a32bc763e4c880ddc3d2
David Lemayian <david@codeforafrica.org>
2014-10-06 17:45:27 +0300

    Automatic category assignment FTW!

--- app/models/Category.php
+++ app/models/Category.php
@@ -14,15 +14,46 @@ class Category extends Eloquent {
 
       });
 
+      Category::saving(function($category)
+      {
+
+      });
+
       Category::created(function($category)
       {
-        // Queue::push('CategoryQueue', array('category' => $category));
+        $cat_old = Category::find($category->id);
+        Queue::push('CategoryQueue', array(
+          'cat_id' => $category->id,
+          'cat_new' => $category,
+          'cat_old' => $cat_old,
+          'new' => true
+        ));
+      });
+
+      Category::updating(function($category)
+      {
+        $cat_old = Category::find($category->id);
+        Queue::push('CategoryQueue', array(
+          'cat_id' => $category->id,
+          'cat_new' => $category,
+          'cat_old' => $cat_old,
+          'new' => false
+        ));
+      });
+      Category::deleted(function($category)
+      {
+        self::keywordUnAssign($category);
       });
     }
 
     function projects()
     {
       return $this->belongsToMany('Project');
+    }
+
+    static function keywordUnAssign ( $category )
+    {
+      DB::table('project_category')->where('category_id', $category->id)->delete();
     }
 
 }
---
+++ app/models/CategoryQueue.php
@@ -0,0 +1,74 @@
+<?php
+
+class CategoryQueue {
+
+  public function fire($job, $data)
+  {
+    if ($job->attempts() > 3)
+    {
+        $job->delete();
+    }
+
+    $data_obj = json_decode(json_encode($data), FALSE);
+    $cat_id = $data_obj->cat_id;
+    $cat_old = $data_obj->cat_old;
+    $cat_new = $data_obj->cat_new;
+
+    if ( $data['new'] ) {
+      // New Category
+      // Assign if is not empty keywords
+      if ( $cat_new->kewords != '' && !$cat_new->kewords ) {
+        self::keywordAssign( $cat_new );
+      }
+    } else {
+
+      // Category update
+      if ( $cat_new->keywords != $cat_old->keywords ){
+        // If old + new keywords don't match, Unassign
+        Category::keywordUnAssign( $cat_new );
+
+        if ( $cat_new->keywords != '' && $cat_new->keywords != NULL ) {
+          // If not empty keywords, assign
+          self::keywordAssign( $cat_new );
+        }
+      }
+    }
+
+    Log::info('Category assignment completed.');
+
+    $job->delete();
+
+  }
+
+  function keywordAssign ( $category )
+  {
+    $GLOBALS['cat_keywords'] = explode(",", $category->keywords);
+    $GLOBALS['cat_id'] = $category->id;
+    Project::chunk(8000, function($projects)
+    {
+      $keywords = $GLOBALS['cat_keywords'];
+
+      foreach ($projects as $project)
+      {
+        //
+
+        foreach ( $keywords as $keyword ) {
+          $in_title  = stripos( $project->title, $keyword );
+          $in_desc   = stripos( $project->description, $keyword );
+          $in_sector = stripos( $project->status, $keyword );
+
+          // If keyword found
+          if ($in_title !== false || $in_desc !== false || $in_sector !== false) {
+            DB::table('project_category')->insert(
+              array(
+                'project_id' => $project->id,
+                'category_id' => $GLOBALS['cat_id']
+              )
+            );
+          }
+        }
+      }
+    });
+  }
+
+}
