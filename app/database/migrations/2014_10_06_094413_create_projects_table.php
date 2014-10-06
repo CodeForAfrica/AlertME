322d4ef7eaad3e25eef5c371db3590b4cfe32595
David Lemayian <david@codeforafrica.org>
2014-10-06 09:51:18 +0300

    Added basic project table.

---
+++ app/database/migrations/2014_10_06_094413_create_projects_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('projects', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('data_source_id');
+			$table->integer('data_source_sync_id');
+			$table->mediumText('project_id');
+			$table->mediumText('title');
+			$table->mediumText('description');
+			$table->mediumText('geo_type');
+			$table->mediumText('geo_address');
+			$table->mediumText('geo_lat');
+			$table->mediumText('geo_lng');
+			$table->mediumText('status');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('projects');
+	}
+
+}
