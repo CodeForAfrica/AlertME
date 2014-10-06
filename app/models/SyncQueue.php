f060ec775a9263f0a32b44b33a8ed4fde93f3dca
David Lemayian <david@codeforafrica.org>
2014-10-06 10:10:57 +0300

    Basic data fetch on sync.

--- app/models/SyncQueue.php
+++ app/models/SyncQueue.php
@@ -11,13 +11,16 @@ class SyncQueue {
       self::dataSourceSync($sync, $ds_config);
     }
 
+    // Check if all data sources synced successfully
+
+
     $sync->sync_status = 1;
     $sync->save();
 
     $job->delete();
   }
 
-  function dataSourceSync($sync, $ds_config)
+  function dataSourceSync( $sync, $ds_config )
   {
     $ds_sync = new DataSourceSync;
     $ds_sync->sync_id = $sync->id;
@@ -25,11 +28,53 @@ class SyncQueue {
     $ds_sync->sync_status = 2;
     $ds_sync->save();
 
+    // Fetch Data
+    $csv = self::fetchData($ds_sync);
+    if(!$csv) {
+      $ds_sync->sync_status = 0;
+      $ds_sync->save();
+      return;
+    }
 
+    // Check column change
+    if($csv[0] != json_decode($ds_config->data_source_columns)){
+      $ds_sync->sync_status = 0;
+      $ds_sync->save();
+      return;
+    }
 
 
     $ds_sync->sync_status = 1;
     $ds_sync->save();
+  }
+
+  function fetchData( $ds_sync )
+  {
+    $datasource = DataSource::find( $ds_sync->data_source_id );
+
+    if(!filter_var($datasource->url, FILTER_VALIDATE_URL))
+    {
+      // Not a URL
+      if (! file_exists ( $datasource->url)) return false;
+    }
+    else
+    {
+      // Is a URL
+
+      $file_headers = @get_headers($datasource->url);
+
+      if($file_headers[0] == 'HTTP/1.0 404 Not Found'){
+        // echo "The file $filename does not exist";
+        return false;
+      } else if ($file_headers[0] == 'HTTP/1.0 302 Found' && $file_headers[7] == 'HTTP/1.0 404 Not Found'){
+        // echo "The file $filename does not exist, and I got redirected to a custom 404 page..";
+        return false;
+      }
+    }
+
+    $csv = array_map('str_getcsv', file($datasource->url));
+
+    return $csv;
   }
 
 }
