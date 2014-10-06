4f827aaa8522f397cd85a6500ab85cdc43f1a023
David Lemayian <david@codeforafrica.org>
2014-10-06 20:34:56 +0300

    Added geo apis table.

---
+++ app/database/migrations/2014_10_06_202914_create_geo_apis_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGeoApisTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('geo_apis', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->string('url')->nullable();
+			$table->string('key')->nullable();
+			$table->timestamps();
+		});
+
+		// Add user using Eloquent
+		$geo_api = new GeoApi;
+		$geo_api->name = 'Google Geocoding API';
+		$geo_api->url = 'https://developers.google.com/maps/documentation/geocoding/';
+		$geo_api->save();
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('geo_apis');
+	}
+
+}
---
+++ app/models/GeoApi.php
@@ -0,0 +1,18 @@
+<?php
+
+class GeoApi extends Eloquent {
+
+    protected $table = 'geo_apis';
+
+    public static function boot()
+    {
+      parent::boot();
+
+      // Setup event bindings...
+      GeoApi::created(function($project)
+      {
+
+      });
+
+    }
+}
