ad26e53c8545d84aa6f88998f107fbce7191a817
David Lemayian <david@codeforafrica.org>
2014-10-07 18:55:02 +0300

    Set map centre

--- public/assets/js/frontend.js
+++ public/assets/js/frontend.js
@@ -1,7 +1,8 @@
 $( document ).ready(function() {
 
   L.mapbox.accessToken = 'pk.eyJ1IjoiY29kZWZvcmFmcmljYSIsImEiOiJVLXZVVUtnIn0.JjVvqHKBGQTNpuDMJtZ8Qg';
-  var map = L.mapbox.map('map', 'codeforafrica.ji193j10');
+  var map = L.mapbox.map('map', 'codeforafrica.ji193j10')
+    .setView([-28.4792625, 24.6727135], 5);
 
   map.scrollWheelZoom.disable();
 
