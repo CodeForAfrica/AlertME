56b1fa4eda7cf43f5415d4bf340c61805a982f5f
David Lemayian <david@codeforafrica.org>
2014-11-01 19:40:04 +0300

    Force secure unless local

--- app/filters.php
+++ app/filters.php
@@ -18,6 +18,11 @@ App::before(function($request)
   header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
   header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
   header('Access-Control-Allow-Credentials: true');
+
+  if( ! Request::secure() && App::environment() !== 'local' )
+  {
+     return Redirect::secure(Request::path());
+  }
 });
 
 
