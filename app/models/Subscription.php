86583154f417c195f3d97cb213c018d955a52d26
David Lemayian <david@codeforafrica.org>
2014-11-11 22:45:09 +0300

    Updated subscription e-mail look.

--- app/models/Subscription.php
+++ app/models/Subscription.php
@@ -14,21 +14,48 @@ class Subscription extends Eloquent {
       Subscription::created(function($subscription)
       {
         $user = DB::table('users')->where('id', $subscription->user_id)->first();
-        $data = array(
-          'subscription' => $subscription,
-          'user' => $user,
-          'confirm_token' => $subscription->confirm_token,
-          'confirm_link' => link_to('subscriptions/'.$subscription->confirm_token, 'link', null, true),
-          'confirm_url' => secure_asset('subscriptions/'.$subscription->confirm_token)
+        $confirm_link = link_to('subscriptions/'.$subscription->confirm_token, 'link', null, true);
+        $confirm_url = secure_asset('subscriptions/'.$subscription->confirm_token);
+        $map_image_link = 'https://api.tiles.mapbox.com/v4/codeforafrica.ji193j10'.
+          '/geojson('.urlencode($subscription->geojson).')'.
+          '/auto/600x250.png?'.
+          'access_token=pk.eyJ1IjoiY29kZWZvcmFmcmljYSIsImEiOiJVLXZVVUtnIn0.JjVvqHKBGQTNpuDMJtZ8Qg';
+        $data = compact(
+          'subscription', 'user',
+          'map_image_link', 'confirm_link', 'confirm_url'
         );
 
         Mail::queue('emails.subscription.new', $data, function($message) use ($user)
         {
-          $message->to($user->email)->subject('Confirm Subscription! | #GreenAlert');
+          $message->to($user->email)->subject('#GreenAlert | Confirm Subscription!');
         });
 
       });
 
+    }
+
+    public function alerts()
+    {
+        return $this->belongsToMany('Alert', 'subscription_alert');
+    }
+
+
+    // Accessors & Mutators
+    public function getSwLatAttribute($value)
+    {
+        return floatval($value);
+    }
+    public function getSwLngAttribute($value)
+    {
+        return floatval($value);
+    }
+    public function getNeLatAttribute($value)
+    {
+        return floatval($value);
+    }
+    public function getNeLngAttribute($value)
+    {
+        return floatval($value);
     }
 
 }
