5d3de8fd17efb993681a37530d51105be596fa0a
David Lemayian <david@codeforafrica.org>
2014-11-12 20:05:24 +0300

    Another one, another one.

--- app/database/migrations/2014_10_27_215958_include_subscribers_in_users_table.php
+++ app/database/migrations/2014_10_27_215958_include_subscribers_in_users_table.php
@@ -5,44 +5,45 @@ use Illuminate\Database\Migrations\Migration;
 
 class IncludeSubscribersInUsersTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('users', function(Blueprint $table)
-		{
-			//
-			$table->unique('email');
-			$table->index('email');
-			$table->integer('role_id')->default(0)->after('email');
-			$table->integer('subscriptions')->default(0)->after('role_id');
-		});
-		DB::statement('ALTER TABLE `users` MODIFY `username` VARCHAR(64) NULL;');
-		DB::statement('ALTER TABLE `users` MODIFY `password` VARCHAR(64) NULL;');
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('users', function(Blueprint $table)
+    {
+      //
+      $table->unique('email');
+      $table->index('email');
+      $table->integer('role_id')->default(0)->after('email');
+      $table->integer('subscriptions')->default(0)->after('role_id');
+    });
+    DB::statement('ALTER TABLE `users` MODIFY `username` VARCHAR(64) NULL;');
+    DB::statement('ALTER TABLE `users` MODIFY `password` VARCHAR(64) NULL;');
 
-		$user = User::find(1);
-		$user->role_id = 1;
-		$user->save();
-	}
+    // Update first user to have super admin role
+    DB::table('users')
+      ->where('id', 1)
+      ->update(array('role_id' => 1));
+  }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('users', function(Blueprint $table)
-		{
-			//
-			$table->dropUnique('users_email_unique');
-			$table->dropIndex('users_email_index');
-			$table->dropColumn(array('role_id', 'subscriptions'));
-		});
-		
-	}
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::table('users', function(Blueprint $table)
+    {
+      //
+      $table->dropUnique('users_email_unique');
+      $table->dropIndex('users_email_index');
+      $table->dropColumn(array('role_id', 'subscriptions'));
+    });
+    
+  }
 
 }
