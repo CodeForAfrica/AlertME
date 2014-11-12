c5c2b59d03f5a4a1f96404cd631dd80ce3c2fd5e
David Lemayian <david@codeforafrica.org>
2014-11-12 19:58:53 +0300

    Use query builder instead of Eloquent to add first user.

--- app/database/migrations/2014_10_01_024415_create_users_table.php
+++ app/database/migrations/2014_10_01_024415_create_users_table.php
@@ -5,39 +5,41 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('users', function(Blueprint $table)
-		{
-			$table->increments('id');
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('users', function(Blueprint $table)
+    {
+      $table->increments('id');
       $table->string('username', 64);
       $table->string('password', 64);
       $table->string('email', 128);
       $table->string('remember_token', 100)->nullable();
       $table->timestamps();
-		});
+    });
 
-		// Add user using Eloquent
-    $user = new User;
-    $user->username = 'admin';
-    $user->email = 'admin@localhost';
-    $user->password = Hash::make('password');
-    $user->save();
-	}
+    // Add first user
+    DB::table('users')->insert(
+      array(
+        'username' => 'admin',
+        'email' => 'admin@localhost',
+        'password' => Hash::make('password')
+      )
+    );
+  }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('users');
-	}
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::drop('users');
+  }
 
 }
