e6939afadae5e0fd8e011e1f5b66cb2c59843c2c
David Lemayian <david@codeforafrica.org>
2014-12-21 22:16:57 +0300

    Pahali categories using new function arguments

--- public/assets/js/frontend/map-categories.js
+++ public/assets/js/frontend/map-categories.js
@@ -11,7 +11,7 @@ $( document ).ready(function() {
   $('.cat-all').addClass('active');
 
   $('.cat-sel').click(function (){
-    pahali.map.filter_by_category( $(this).attr('data-cat-id') );
+    pahali.map.filter_by_category( $(this).attr('data-cat-id'), pahali.categories, pahali.projects );
   });
   
 });
