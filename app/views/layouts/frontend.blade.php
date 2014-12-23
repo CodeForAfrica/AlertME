df5eafa2590f270473a4391c1e7d98e35941a9c2
David Lemayian <david@codeforafrica.org>
2014-12-23 16:35:36 +0300

    Updated to classic Uservoice

--- app/views/layouts/frontend.blade.php
+++ app/views/layouts/frontend.blade.php
@@ -88,47 +88,22 @@
     </div>
   </footer>
 
+  <!-- UserVoice JavaScript SDK (only needed once on a page) -->
+<script>(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/bFsioBFsYYe3fBl1hFBFOQ.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})()</script>
+
+  <!-- A tab to launch the Classic Widget -->
   <script>
-  // Include the UserVoice JavaScript SDK (only needed once on a page)
-  UserVoice=window.UserVoice||[];(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/bFsioBFsYYe3fBl1hFBFOQ.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})();
-
-  //
-  // UserVoice Javascript SDK developer documentation:
-  // https://www.uservoice.com/o/javascript-sdk
-  //
-
-  // Set colors
-  UserVoice.push(['set', {
-    accent_color: '#6aba2e',
-    trigger_color: 'white',
-    trigger_background_color: 'rgba(46, 49, 51, 0.6)'
+  UserVoice = window.UserVoice || [];
+  UserVoice.push(['showTab', 'classic_widget', {
+    mode: 'full',
+    primary_color: '#1abc9c',
+    link_color: '#2980b9',
+    default_mode: 'support',
+    forum_id: 277432,
+    tab_label: 'Feedback & Support',
+    tab_color: '#1abc9c',
+    tab_position: 'middle-right',
+    tab_inverted: false
   }]);
-
-  // Identify the user and pass traits
-  // To enable, replace sample data with actual user traits and uncomment the line
-  UserVoice.push(['identify', {
-    //email:      'john.doe@example.com', // User’s email address
-    //name:       'John Doe', // User’s real name
-    //created_at: 1364406966, // Unix timestamp for the date the user signed up
-    //id:         123, // Optional: Unique id of the user (if set, this should not change)
-    //type:       'Owner', // Optional: segment your users by type
-    //account: {
-    //  id:           123, // Optional: associate multiple users with a single account
-    //  name:         'Acme, Co.', // Account name
-    //  created_at:   1364406966, // Unix timestamp for the date the account was created
-    //  monthly_rate: 9.99, // Decimal; monthly rate of the account
-    //  ltv:          1495.00, // Decimal; lifetime value of the account
-    //  plan:         'Enhanced' // Plan name for the account
-    //}
-  }]);
-
-  // Add default trigger to the bottom-right corner of the window:
-  UserVoice.push(['addTrigger', { mode: 'contact', trigger_position: 'bottom-right' }]);
-
-  // Or, use your own custom trigger:
-  //UserVoice.push(['addTrigger', '#id', { mode: 'contact' }]);
-
-  // Autoprompt for Satisfaction and SmartVote (only displayed under certain conditions)
-  UserVoice.push(['autoprompt', {}]);
   </script>
 @stop
