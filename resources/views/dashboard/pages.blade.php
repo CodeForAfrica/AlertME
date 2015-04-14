67c19e1f11ce506c7b01d80d9d92ca2f8517a9ea
David Lemayian <david@codeforafrica.org>
2015-04-14 18:14:43 +0300

    Dashboard pages working.

--- resources/views/dashboard/pages.blade.php
+++ resources/views/dashboard/pages.blade.php
@@ -2,77 +2,85 @@
 
 @section('content')
 
-<div class="pages">
-  {{ Form::open(array('url' => 'dashboard/pages')) }}
+  <div class="pages">
 
-  <h5>Pages
-    <button type="submit" class="btn btn-primary btn-embossed btn-wide"
-      style="margin-left: 30px;">Save Changes</button>
-  </h5>
-  <hr/>
+    <form action="{{ secure_url('dashboard/pages') }}" method="POST">
 
-  <!-- Nav tabs -->
-  <ul class="nav nav-tabs" role="tablist">
-    <li role="presentation" class="active"><a href="#home" role="tab" data-toggle="tab">Home</a></li>
-    <li role="presentation"><a href="#about" role="tab" data-toggle="tab">About</a></li>
-  </ul><br/>
+      <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-  <!-- Tab panes -->
-  <div class="tab-content">
-    <!-- Home -->
-    <div role="tabpanel" class="tab-pane active" id="home">
-      <div class="row">
-        <div class="col-md-3">
-          <p class="text-muted"><em>This is the page that welcomes your users to the platform.</em></p>
-          <p class="text-muted"><a href="/" target="_blank"><span class="fui-link"></span> Link</a></p>
-        </div>
-        <div class="col-md-8 col-md-offset-1">
-          <input id="home[banner][title]" name="home[banner][title]" type="text" placeholder="Title"
-            class="form-control input-hg flat text-center"
-            value="{{ $home->data->banner->title }}" />
-          <input id="home[banner][description]" name="home[banner][description]" type="text" placeholder="Title"
-            class="form-control flat text-center"
-            value="{{ $home->data->banner->description }}" />
+      <h5>Pages
+        <button type="submit" class="btn btn-primary btn-embossed btn-wide"
+                style="margin-left: 30px;">Save Changes
+        </button>
+      </h5>
+      <hr/>
 
-          <hr/>
+      <!-- Nav tabs -->
+      <ul class="nav nav-tabs" role="tablist">
+        <li role="presentation" class="active"><a href="#home" role="tab" data-toggle="tab">Home</a></li>
+        <li role="presentation"><a href="#about" role="tab" data-toggle="tab">About</a></li>
+      </ul>
+      <br/>
 
-          <input id="home[how][title]" name="home[how][title]" type="text" placeholder="Title"
-            class="form-control input-hg flat text-center"
-            value="{{ $home->data->how->title }}" />
+      <!-- Tab panes -->
+      <div class="tab-content">
+        <!-- Home -->
+        <div role="tabpanel" class="tab-pane active" id="home">
           <div class="row">
-            @foreach($home->data->how->blurbs as $key => $blurb)
-              <div class="col-sm-4">
+            <div class="col-md-3">
+              <p class="text-muted"><em>This is the page that welcomes your users to the platform.</em></p>
+
+              <p class="text-muted"><a href="/" target="_blank"><span class="fui-link"></span> Link</a></p>
+            </div>
+            <div class="col-md-8 col-md-offset-1">
+              <input id="home[banner][title]" name="home[banner][title]" type="text" placeholder="Title"
+                     class="form-control input-hg flat text-center"
+                     value="{{ $home->data->banner->title }}"/>
+              <input id="home[banner][description]" name="home[banner][description]" type="text" placeholder="Title"
+                     class="form-control flat text-center"
+                     value="{{ $home->data->banner->description }}"/>
+
+              <hr/>
+
+              <input id="home[how][title]" name="home[how][title]" type="text" placeholder="Title"
+                     class="form-control input-hg flat text-center"
+                     value="{{ $home->data->how->title }}"/>
+
+              <div class="row">
+                @foreach($home->data->how->blurbs as $key => $blurb)
+                  <div class="col-sm-4">
                 <textarea id="home[how][blurbs][{{$key}}][description]"
-                  name="home[how][blurbs][{{$key}}][description]" placeholder="Description"
-                  class="form-control flat pages-desc text-center">{{ $blurb->description }}</textarea>
+                          name="home[how][blurbs][{{$key}}][description]" placeholder="Description"
+                          class="form-control flat pages-desc text-center">{{ $blurb->description }}</textarea>
+                  </div>
+                @endforeach
               </div>
-            @endforeach
+
+            </div>
           </div>
-
         </div>
-      </div>
-    </div>
 
-    <!-- About -->
-    <div role="tabpanel" class="tab-pane" id="about">
-      <div class="row">
-        <div class="col-md-3">
-          <p class="text-muted"><em>This is the page that speaks to what the platform is about.</em></p>
-          <p class="text-muted"><a href="/about" target="_blank"><span class="fui-link"></span> Link</a></p>
-        </div>
-        <div class="col-md-8 col-md-offset-1">
-          <input id="about[title]" name="about[title]" type="text" placeholder="Title"
-            class="form-control input-hg flat" value="{{ $about->data->title }}" />
+        <!-- About -->
+        <div role="tabpanel" class="tab-pane" id="about">
+          <div class="row">
+            <div class="col-md-3">
+              <p class="text-muted"><em>This is the page that speaks to what the platform is about.</em></p>
+
+              <p class="text-muted"><a href="/about" target="_blank"><span class="fui-link"></span> Link</a></p>
+            </div>
+            <div class="col-md-8 col-md-offset-1">
+              <input id="about[title]" name="about[title]" type="text" placeholder="Title"
+                     class="form-control input-hg flat" value="{{ $about->data->title }}"/>
           <textarea id="about[description]" name="about[description]" class="form-control flat pages-desc"
-            rows="4" placeholder="Description">{{ $about->data->description }}</textarea>
+                    rows="4" placeholder="Description">{{ $about->data->description }}</textarea>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
 
-  {{ Form::close() }}
+    </form>
 
-</div> <!-- /.pages -->
+  </div> <!-- /.pages -->
 
 @stop
 
