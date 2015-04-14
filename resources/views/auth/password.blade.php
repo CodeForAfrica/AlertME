8e40670913b50ab9279f6380b7b05276365350e1
David Lemayian <david@codeforafrica.org>
2015-04-14 11:38:17 +0300

    Updated auth designs.

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -1,50 +1,78 @@
-@extends('app')
+@extends('layouts.base')
 
+@section('title')
+  Reset Password
+@stop
+
+{{-- Content --}}
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Reset Password</div>
-				<div class="panel-body">
-					@if (session('status'))
-						<div class="alert alert-success">
-							{{ session('status') }}
-						</div>
-					@endif
 
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
+  <div class="container">
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+    <div class="row">
+      <div class="col-md-4 col-md-offset-4">
+        <h4>Reset Password</h4>
+      </div>
+    </div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
+    <div class="row">
+      <div class="col-md-4 col-md-offset-4">
+        <div class="login-form">
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Send Password Reset Link
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-@endsection
+          @if (session('status'))
+            <div class="alert alert-success">
+              {{ session('status') }}
+            </div>
+          @endif
+
+          @if (count($errors) > 0)
+            <div class="alert alert-danger">
+              <strong>Whoops!</strong> There were some problems with your input.<br><br>
+              <ul>
+                @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+                @endforeach
+              </ul>
+            </div>
+          @endif
+
+          <form role="form" method="POST" action="{{ secure_url('/password/email') }}">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+            <div class="form-group {{{ $errors->has('email') ? 'has-error' : '' }}}">
+              <input type="email" name="email" class="form-control login-field"
+                     value="{{ Input::old('email') }}" placeholder="Email Address" id="login-email">
+              <label class="login-field-icon fui-mail" for="login-email"></label>
+            </div>
+            <button class="btn btn-primary btn-embossed btn-lg btn-block" type="submit">Send Password Reset Link
+            </button>
+          </form>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+@stop
+
+@section('stylesheets')
+  <style>
+    body {
+      padding-top: 100px;
+      padding-bottom: 40px;
+      background-color: #1abc9c;
+    }
+
+    .has-error .form-control, .has-error .select2-search input[type=text] {
+      color: #e74c3c;
+      border-color: #e74c3c !important;
+      box-shadow: none;
+    }
+  </style>
+@stop
+
+@section('scripts')
+  <script>
+    $(':checkbox').radiocheck();
+  </script>
+@stop
--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -1,59 +1,84 @@
-@extends('app')
+@extends('layouts.base')
 
+@section('title')
+  Reset Password
+@stop
+
+{{-- Content --}}
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Reset Password</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-						<input type="hidden" name="token" value="{{ $token }}">
+  <div class="container">
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
+    <div class="row">
+      <div class="col-md-4 col-md-offset-4">
+        <h4>Reset Password</h4>
+      </div>
+    </div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
+    <div class="row">
+      <div class="col-md-4 col-md-offset-4">
+        <div class="login-form">
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Confirm Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password_confirmation">
-							</div>
-						</div>
+          @if (count($errors) > 0)
+            <div class="alert alert-danger">
+              <strong>Whoops!</strong> There were some problems with your input.<br><br>
+              <ul>
+                @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+                @endforeach
+              </ul>
+            </div>
+          @endif
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Reset Password
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-@endsection
+          <form role="form" method="POST" action="{{ url('/password/reset') }}">
+
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <input type="hidden" name="token" value="{{ $token }}">
+
+            <div class="form-group {{{ $errors->has('email') ? 'has-error' : '' }}}">
+              <input type="email" name="email" class="form-control login-field"
+                     value="{{ Input::old('email') }}" placeholder="Email Address" id="login-email">
+              <label class="login-field-icon fui-mail" for="login-email"></label>
+            </div>
+            <div class="form-group {{{ $errors->has('password') ? 'has-error' : '' }}}">
+              <input type="password" name="password" class="form-control login-field"
+                     value="" placeholder="Password" id="login-pass">
+              <label class="login-field-icon fui-lock" for="login-pass"></label>
+            </div>
+            <div class="form-group {{{ $errors->has('password_confirmation') ? 'has-error' : '' }}}">
+              <input type="password" name="password_confirmation" class="form-control login-field"
+                     value="" placeholder="Confirm Password" id="login-pass-2">
+              <label class="login-field-icon fui-lock" for="login-pass-2"></label>
+            </div>
+            <button class="btn btn-primary btn-embossed btn-lg btn-block" type="submit">Reset Password</button>
+
+          </form>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+@stop
+
+@section('stylesheets')
+  <style>
+    body {
+      padding-top: 100px;
+      padding-bottom: 40px;
+      background-color: #1abc9c;
+    }
+
+    .has-error .form-control, .has-error .select2-search input[type=text] {
+      color: #e74c3c;
+      border-color: #e74c3c !important;
+      box-shadow: none;
+    }
+  </style>
+@stop
+
+@section('scripts')
+  <script>
+    $(':checkbox').radiocheck();
+  </script>
+@stop
