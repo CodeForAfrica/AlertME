fd87e8dc46902c70e94c4faaf27e3d7c920e125e
David Lemayian <david@codeforafrica.org>
2015-04-17 13:21:34 +0300

    Added a TODO

--- app/Category.php
+++ app/Category.php
@@ -12,28 +12,27 @@ class Category extends Model {
         parent::boot();
 
         // Setup event bindings...
-        Category::creating(function($category)
-        {
+        Category::creating(function ($category) {
 
         });
 
-        Category::saving(function($category)
-        {
+        Category::saving(function ($category) {
 
         });
 
-        Category::created(function($category)
-        {
+        Category::created(function ($category) {
             \Queue::push(new CategoryQueue($category->id));
         });
 
-        Category::updated(function($category)
-        {
+        Category::updated(function ($category) {
             \Queue::push(new CategoryQueue($category->id));
         });
 
-        Category::deleted(function($category)
-        {
+        Category::deleting(function ($category) {
+            // TODO: Check whether there is a queue job with this - Locking?
+        });
+
+        Category::deleted(function ($category) {
             \DB::table('project_category')
                 ->where('category_id', $category->id)
                 ->delete();
@@ -61,16 +60,16 @@ class Category extends Model {
             'geocodes.status as geo_status');
         $projects_lat_lng = \DB::table('projects')
             ->where('geo_type', 'lat_lng')
-            ->select( $sel_cols_projects )->get();
+            ->select($sel_cols_projects)->get();
         $projects_address = \DB::table('projects')
             ->join('geocodes', 'projects.geo_address', '=', 'geocodes.address')
             ->where('projects.geo_type', '=', 'address')
-            ->select( array_merge($sel_cols_projects, $sel_cols_geocodes) )->get();
+            ->select(array_merge($sel_cols_projects, $sel_cols_geocodes))->get();
         $projects = array_merge($projects_lat_lng, $projects_address);
 
         $projects_id = array();
         foreach ($projects as $project) {
-            array_push( $projects_id, $project->id);
+            array_push($projects_id, $project->id);
         }
         $projects_categories = \DB::table('project_category')
             ->whereIn('project_id', $projects_id)
@@ -79,7 +78,7 @@ class Category extends Model {
             ->get();
         $category_ids = array();
         foreach ($projects_categories as $projects_category) {
-            array_push( $category_ids, $projects_category->category_id);
+            array_push($category_ids, $projects_category->category_id);
         }
 
         if (count($category_ids) == 0) {
