6d281218c66e6fe8311cfdcf6b39664fd35803ab
David Lemayian <dlemayian@gmail.com>
2017-05-31 10:34:19 +0300

    Reduce the about page width.

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -6,33 +6,35 @@
 
   <div class="about" style="padding-bottom:60px;">
 
-    <div class="container ">
-      <div class="page-header">
-        <h1>{{ $about->data->title == '' ? 'About' : $about->data->title }}</h1>
+    <div class="container">
+      <div style="margin: 0 auto; max-width: 800px;">
+        <div class="page-header">
+          <h1>{{ $about->data->title == '' ? 'About' : $about->data->title }}</h1>
+        </div>
+
+        @if ( $about->data->description == '' )
+
+          <p>South African law says planned development projects, including mines,
+            dams, power stations, roads and landfill sites, need to have their
+            environmental impacts assessed bofore they can go ahead.</p>
+          <p>#AlertME helps you to find out what Environmental Impact Assessments
+            (EIAs) are happening in your area.</p>
+          <p>Find your location to see the details of an EIA: its official ID, the
+            project description, status of the development, and the government body
+            responsible for authorising and monitoring the development.</p>
+          <p>You can keep up to date with the changing status of EIAs that interest
+            you by registering for personalised alerts. We will send you real-time
+            notifications by email or SMS.</p>
+          <p>And you can help keep the developers accountable by joining and
+            participating in our community network.</p>
+
+        @else
+
+          {!! Markdown::convertToHtml($about->data->description) !!}
+
+        @endif
       </div>
-
-      @if ( $about->data->description == '' )
-
-        <p>South African law says planned development projects, including mines,
-          dams, power stations, roads and landfill sites, need to have their
-          environmental impacts assessed bofore they can go ahead.</p>
-        <p>#AlertME helps you to find out what Environmental Impact Assessments
-          (EIAs) are happening in your area.</p>
-        <p>Find your location to see the details of an EIA: its official ID, the
-          project description, status of the development, and the government body
-          responsible for authorising and monitoring the development.</p>
-        <p>You can keep up to date with the changing status of EIAs that interest
-          you by registering for personalised alerts. We will send you real-time
-          notifications by email or SMS.</p>
-        <p>And you can help keep the developers accountable by joining and
-          participating in our community network.</p>
-
-      @else
-
-        {!! Markdown::convertToHtml($about->data->description) !!}
-
-      @endif
-
+      
       <hr style="margin:50px 0;"/>
 
       @include('home.snippets.partners')
