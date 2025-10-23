4a772bd827c5fc40a1bceade2d51ef487f7ac9fa
Vincent Otieno <vincentjayden49@gmail.com>
2025-10-23 09:38:07 +0300

    chore: Update database configuration for MySQL 8.0 compatibility and add upgrade issue report

--- config/database.php
+++ config/database.php
@@ -50,8 +50,11 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'strict' => true,
+            'strict' => false,
             'engine' => null,
+            'modes' => [
+                'NO_ENGINE_SUBSTITUTION',
+            ],
         ],
 
         'pgsql' => [
